d44b8bbb5a241fd2c9d954b5125998109c4718f3
Uptech Team <uptechteam@Uptechs-MacBook-Pro-2.local>
2019-10-28 14:22:01 +0200

    Updates

---
+++ precache-manifest.7a405745a1c7f9bf2b613976de49845f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "18bfaccdb60d823046763694f16c6e5d",
+    "url": "/test-auth-app/index.html"
+  },
+  {
+    "revision": "c11b67471d58c092a322",
+    "url": "/test-auth-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "c1302f899375b0406c53",
+    "url": "/test-auth-app/static/js/2.e3ce1568.chunk.js"
+  },
+  {
+    "revision": "c11b67471d58c092a322",
+    "url": "/test-auth-app/static/js/main.377f480b.chunk.js"
+  },
+  {
+    "revision": "822dea387602c221d664",
+    "url": "/test-auth-app/static/js/runtime-main.516e1223.js"
+  }
+]);
